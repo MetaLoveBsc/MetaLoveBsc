187db80a1c94753e2209536192deee71f4c549e6
MetaLoveBsc <100236184+MetaLoveBsc@users.noreply.github.com>
2022-02-22 20:52:54 +0100

    Add files via upload

---
+++ MasterChefMLC.sol
@@ -0,0 +1,379 @@
+pragma solidity 0.6.12;
+
+import './lib/SafeMath.sol';
+import './lib/IBEP20.sol';
+import './lib/SafeBEP20.sol';
+import './lib/Ownable.sol';
+import './lib/ReentrancyGuard.sol';
+
+import './MetaLoveCoinToken.sol';
+// import "@nomiclabs/buidler/console.sol";
+
+// MasterChef is the master of MLC. He can make MLC and he is a fair guy.
+//
+// Note that it's ownable and the owner wields tremendous power. The ownership
+// will be transferred to a governance smart contract once MLC is sufficiently
+// distributed and the community can show to govern itself.
+//
+// Have fun reading it. Hopefully it's bug-free. God bless.
+contract MasterChefMLC is Ownable, ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeBEP20 for IBEP20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        uint256 rewardLockedUp;  // Reward locked up.
+        uint256 nextHarvestUntil; // When can the user harvest again.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of MLCs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accMLCPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accMLCPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IBEP20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. MLCs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that MLCs distribution occurs.
+        uint256 accMLCPerShare; // Accumulated MLCs per share, times 1e12. See below.
+        uint16 depositFeeBP; // Deposit fee in basis points
+        uint256 harvestInterval;  // Harvest interval in seconds
+    }
+
+    // The MLC TOKEN!
+    MetaLoveCoinToken public mlc;
+    // MLC tokens created per block.
+    uint256 public mlcPerBlock;
+    // Deposit Fee address
+    address public feeAddress;
+    // Bonus muliplier for early mlc makers.
+    uint256 public BONUS_MULTIPLIER = 1;
+    // Max harvest interval: 1 days.
+    uint256 public constant MAXIMUM_HARVEST_INTERVAL = 4 hours;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when MLC mining starts.
+    uint256 public startBlock;
+    // The block number when Reward mining ends.
+    uint256 public bonusEndBlock;
+    // Total locked up rewards
+    uint256 public totalLockedUpRewards;
+    // Referral Bonus in basis points. Initially set to 3%
+    uint256 public refBonusBP = 300;
+    // Max referral commission rate: 5%.
+    uint16 public constant MAXIMUM_REFERRAL_BP = 500;
+    // Referral Mapping
+    mapping(address => address) public referrers; // account_address -> referrer_address
+    mapping(address => uint256) public referredCount; // referrer_address -> num_of_referred
+    // Max deposit fee: 5%.
+    uint16 public constant MAXIMUM_DEPOSIT_FEE_BP = 500;
+    // Pool Exists Mapper
+    mapping(IBEP20 => bool) public poolExistence;
+    // Pool ID Tracker Mapper
+    mapping(IBEP20 => uint256) public poolIdForLpAddress;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event SetFeeAddress(address indexed user, address indexed _feeAddress);
+    event EmissionRateUpdated(address indexed caller, uint256 previousAmount, uint256 newAmount);
+    event RewardLockedUp(address indexed user, uint256 indexed pid, uint256 amountLockedUp);
+    event Referral(address indexed _referrer, address indexed _user);
+    event ReferralPaid(address indexed _user, address indexed _userTo, uint256 _reward);
+    event ReferralBonusBpChanged(uint256 _oldBp, uint256 _newBp);
+
+    constructor(
+        MetaLoveCoinToken _mlc,
+        address _feeAddress,
+        uint256 _mlcPerBlock,
+        uint256 _startBlock,
+        uint256 _bonusEndBlock
+    ) public {
+        mlc = _mlc;
+        feeAddress = _feeAddress;
+        mlcPerBlock = _mlcPerBlock;
+        startBlock = _startBlock;
+        bonusEndBlock = _bonusEndBlock;
+
+        // staking pool
+        poolInfo.push(PoolInfo({
+            lpToken: _mlc,
+            allocPoint: 1000,
+            lastRewardBlock: startBlock,
+            accMLCPerShare: 0,
+            depositFeeBP: 0,
+            harvestInterval: 0
+        }));
+
+        totalAllocPoint = 1000;
+
+    }
+
+    function updateMultiplier(uint256 multiplierNumber) public onlyOwner {
+        BONUS_MULTIPLIER = multiplierNumber;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Modifier to check Duplicate pools
+    modifier nonDuplicated(IBEP20 _lpToken) {
+        require(poolExistence[_lpToken] == false, "nonDuplicated: duplicated");
+        _;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    function add(uint256 _allocPoint, IBEP20 _lpToken, uint16 _depositFeeBP, uint256 _harvestInterval, bool _withUpdate) public onlyOwner nonDuplicated(_lpToken) {
+        require(_depositFeeBP <= MAXIMUM_DEPOSIT_FEE_BP, "set: invalid deposit fee basis points");
+        require(_harvestInterval <= MAXIMUM_HARVEST_INTERVAL, "add: invalid harvest interval");
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolExistence[_lpToken] = true;
+        poolInfo.push(PoolInfo({
+        lpToken: _lpToken,
+        allocPoint: _allocPoint,
+        lastRewardBlock: lastRewardBlock,
+        accMLCPerShare: 0,
+        depositFeeBP: _depositFeeBP,
+        harvestInterval: _harvestInterval
+        }));
+    }
+
+    // Update the given pool's MLC allocation point. Can only be called by the owner.
+    function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, uint256 _harvestInterval, bool _withUpdate) public onlyOwner {
+        require(_depositFeeBP <= MAXIMUM_DEPOSIT_FEE_BP, "set: invalid deposit fee basis points");
+        require(_harvestInterval <= MAXIMUM_HARVEST_INTERVAL, "add: invalid harvest interval");
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 prevAllocPoint = poolInfo[_pid].allocPoint;
+        poolInfo[_pid].allocPoint = _allocPoint;
+        poolInfo[_pid].depositFeeBP = _depositFeeBP;
+        poolInfo[_pid].harvestInterval = _harvestInterval;
+        if (prevAllocPoint != _allocPoint) {
+            totalAllocPoint = totalAllocPoint.sub(prevAllocPoint).add(_allocPoint);
+        }
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        if (_to <= bonusEndBlock) {
+            return _to.sub(_from);
+        } else if (_from >= bonusEndBlock) {
+            return 0;
+        } else {
+            return bonusEndBlock.sub(_from);
+        }
+    }
+
+    // View function to see pending MLCs on frontend.
+    function pendingReward(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accMLCPerShare = pool.accMLCPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 mlcReward = multiplier.mul(mlcPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accMLCPerShare = accMLCPerShare.add(mlcReward.mul(1e12).div(lpSupply));
+        }
+        uint256 pending = user.amount.mul(accMLCPerShare).div(1e12).sub(user.rewardDebt);
+        return pending.add(user.rewardLockedUp);
+    }
+
+    // View function to see if user can harvest MLC's.
+    function canHarvest(uint256 _pid, address _user) public view returns (bool) {
+        UserInfo storage user = userInfo[_pid][_user];
+        return block.timestamp >= user.nextHarvestUntil;
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 mlcReward = multiplier.mul(mlcPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        mlc.mint(feeAddress, mlcReward.div(3));
+        mlc.mint(address(this), mlcReward);
+        pool.accMLCPerShare = pool.accMLCPerShare.add(mlcReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to MasterChef for MLC allocation.
+    function deposit(uint256 _pid, uint256 _amount) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        payOrLockupPendingCryptoMLC(_pid);
+        if (_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            if (pool.depositFeeBP > 0) {
+                uint256 depositFee = _amount.mul(pool.depositFeeBP).div(10000);
+                user.amount = user.amount.add(_amount).sub(depositFee);
+                pool.lpToken.safeTransfer(feeAddress, depositFee);
+            } else {
+                user.amount = user.amount.add(_amount);
+            }
+        }
+        user.rewardDebt = user.amount.mul(pool.accMLCPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Deposit LP tokens to MasterChef for MLC allocation with referral.
+    function deposit(uint256 _pid, uint256 _amount, address _referrer) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        payOrLockupPendingCryptoMLC(_pid);
+        if (_amount > 0) {
+            setReferral(msg.sender, _referrer);
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            if (pool.depositFeeBP > 0) {
+                uint256 depositFee = _amount.mul(pool.depositFeeBP).div(10000);
+                user.amount = user.amount.add(_amount).sub(depositFee);
+                pool.lpToken.safeTransfer(feeAddress, depositFee);
+            } else {
+                user.amount = user.amount.add(_amount);
+            }
+        }
+        user.rewardDebt = user.amount.mul(pool.accMLCPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256 _pid, uint256 _amount) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+
+        updatePool(_pid);
+        payOrLockupPendingCryptoMLC(_pid);
+        if(_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accMLCPerShare).div(1e12);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+
+    // Pay or lockup pending MLC's.
+    function payOrLockupPendingCryptoMLC(uint256 _pid) internal {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+
+        if (user.nextHarvestUntil == 0) {
+            user.nextHarvestUntil = block.timestamp.add(pool.harvestInterval);
+        }
+
+        uint256 pending = user.amount.mul(pool.accMLCPerShare).div(1e12).sub(user.rewardDebt);
+        if (canHarvest(_pid, msg.sender)) {
+            if (pending > 0 || user.rewardLockedUp > 0) {
+                uint256 totalRewards = pending.add(user.rewardLockedUp);
+                totalLockedUpRewards = totalLockedUpRewards.sub(user.rewardLockedUp);
+                user.rewardLockedUp = 0;
+                user.nextHarvestUntil = block.timestamp.add(pool.harvestInterval);
+
+                safeMLCTransfer(msg.sender, totalRewards);
+                payReferralCommission(msg.sender, totalRewards);
+            }
+        } else if (pending > 0) {
+            user.rewardLockedUp = user.rewardLockedUp.add(pending);
+            totalLockedUpRewards = totalLockedUpRewards.add(pending);
+            emit RewardLockedUp(msg.sender, _pid, pending);
+        }
+    }
+
+    // Safe mlc transfer function, just in case if rounding error causes pool to not have enough MLCs.
+    function safeMLCTransfer(address _to, uint256 _amount) internal {
+        uint256 mlcBalance = mlc.balanceOf(address(this));
+        if (_amount > mlcBalance) {
+            mlc.transfer(_to, mlcBalance);
+        } else {
+            mlc.transfer(_to, _amount);
+        }
+    }
+
+    // Update fee address by the previous fee address.
+    function setFeeAddress(address _feeAddress) public {
+        require(_feeAddress != address(0), "setFeeAddress: invalid address");
+        require(msg.sender == feeAddress, "setFeeAddress: FORBIDDEN");
+        feeAddress = _feeAddress;
+        emit SetFeeAddress(msg.sender, _feeAddress);
+    }
+
+    // Set Referral Address for a user
+    function setReferral(address _user, address _referrer) internal {
+        if (_referrer == address(_referrer) && referrers[_user] == address(0) && _referrer != address(0) && _referrer != _user) {
+            referrers[_user] = _referrer;
+            referredCount[_referrer] += 1;
+            emit Referral(_user, _referrer);
+        }
+    }
+
+    // Get Referral Address for a Account
+    function getReferral(address _user) public view returns (address) {
+        return referrers[_user];
+    }
+
+    // Pay referral commission to the referrer who referred this user.
+    function payReferralCommission(address _user, uint256 _pending) internal {
+        address referrer = getReferral(_user);
+        if (referrer != address(0) && referrer != _user && refBonusBP > 0) {
+            uint256 refBonusEarned = _pending.mul(refBonusBP).div(10000);
+            mlc.mint(referrer, refBonusEarned);
+            emit ReferralPaid(_user, referrer, refBonusEarned);
+        }
+    }
+
+    // Referral Bonus in basis points.
+    function updateReferralBonusBp(uint256 _newRefBonusBp) public onlyOwner {
+        require(_newRefBonusBp <= MAXIMUM_REFERRAL_BP, "updateRefBonusPercent: invalid referral bonus basis points");
+        require(_newRefBonusBp != refBonusBP, "updateRefBonusPercent: same bonus bp set");
+        uint256 previousRefBonusBP = refBonusBP;
+        refBonusBP = _newRefBonusBp;
+        emit ReferralBonusBpChanged(previousRefBonusBP, _newRefBonusBp);
+    }
+}
